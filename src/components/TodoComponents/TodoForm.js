d19af460ee3cc044320897771bb0d9cc54420d5b
eralpkor <eralp.ny@gmail.com>
2019-09-01 13:15:59 -0400

    todo, todoform, todolist added

---
+++ src/components/TodoComponents/TodoForm.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+
+class TodoForm extends Component {
+  constructor(props) {
+    super(props);
+      this.state = {task: ''};
+      this.handleChange = this.handleChange.bind(this);
+  }
+  handleChange(e) {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  }
+
+  render() {
+    return (
+      <form>
+        <label htmlFor='task'>New Todo</label>
+        <input
+        type='text' 
+        placeholder='New Todo' 
+        id='task'
+        value={this.state.task}
+        onChange={this.handleChange}
+        />
+      </form>
+    )
+  }
+}
--- src/components/TodoComponents/TodoList.js
+++ src/components/TodoComponents/TodoList.js
@@ -1,2 +1,27 @@
 // your components will all go in this `component` directory.
 // feel free to change this component.js into TodoList.js
+import React, { Component } from "react";
+import Todo from "./Todo";
+
+class TodoList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      todos: [{ task: "Walk the fish" }, { task: "Feed the chicken" }]
+    };
+  }
+
+  render() {
+    const todos = this.state.todos.map(todo => {
+      return <Todo task={todo.task} />;
+    });
+
+    return (
+      <div>
+        <ul>{todos}</ul>
+      </div>
+    );
+  }
+}
+
+export default TodoList;
