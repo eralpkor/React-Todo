9a45e7621e6fb9999e0d182d0bf25070d1830dd1
eralpkor <eralp.ny@gmail.com>
2019-09-02 15:32:07 -0400

    added handle update to edit todo function

--- src/components/TodoComponents/Todo.js
+++ src/components/TodoComponents/Todo.js
@@ -1,15 +1,35 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-class Todo extends Component{
+class Todo extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      isEditing: false
+      isEditing: false,
+      task: this.props.task
     };
     this.handleRemove = this.handleRemove.bind(this);
+    this.toggleForm = this.toggleForm.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.handleUpdate = this.handleUpdate.bind(this);
   }
   handleRemove() {
     this.props.removeTodo(this.props.id);
+  }
+
+  toggleForm() {
+    this.setState({ isEditing: !this.state.isEditing });
+  }
+
+  handleUpdate(e) {
+    e.preventDefault(e);
+    //take new task data and pass up to parent
+    this.props.updateTodo(this.props.id, this.state.task);
+    this.setState({ isEditing: false })
+  }
+  handleChange(e) {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
   }
 
   render() {
@@ -17,19 +37,25 @@ class Todo extends Component{
     if (this.state.isEditing) {
       result = (
         <div className="Todo">
-          <form>
-            <input type='text' />
+          <form onSubmit={this.handleUpdate}>
+            <input
+            type="text" 
+            value={this.state.task}
+            name='task'
+            onChange={this.handleChange}  
+            />
+            <button>Save</button>
           </form>
         </div>
-      ) 
-      } else {
-        result= (
-          <div>
-            <button>edit</button>
-            <button onClick={this.handleRemove}>X</button>
-            <li>{this.props.task}</li>
-          </div>
-        )
+      );
+    } else {
+      result = (
+        <div>
+          <button onClick={this.toggleForm}>edit</button>
+          <button onClick={this.handleRemove}>X</button>
+          <li>{this.props.task}</li>
+        </div>
+      );
     }
     return result;
   }
